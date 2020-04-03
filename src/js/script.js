52a3b2148d8188ad2bdaad6c5113b9d45be19bdc
Nikolay <kolyanbodymass@gmail.com>
2020-04-03 17:49:10 +0500

    adapted for width 1200px and 991px

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,29 @@
 $(document).ready(function(){ /*функция, которая подключает js код, когда HTML структура документа готова*/
 	$('.reviews__items').slick({
 		centerMode: true,
-		centerPadding: '60px',
+		centerPadding: '0',
 		slidesToShow: 3,
 		speed: 1200,
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>',
-		responsive: [
-			{
-				breakpoint: 768,
-				settings: {
-				centerMode: true,
-				centerPadding: '40px',
-				slidesToShow: 3
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-				centerMode: true,
-				centerPadding: '40px',
-				slidesToShow: 1
-				}
-			}
-		]
+		// responsive: [
+		// 	{
+		// 		breakpoint: 768,
+		// 		settings: {
+		// 		centerMode: true,
+		// 		centerPadding: '40px',
+		// 		slidesToShow: 3
+		// 		}
+		// 	},
+		// 	{
+		// 		breakpoint: 480,
+		// 		settings: {
+		// 		centerMode: true,
+		// 		centerPadding: '40px',
+		// 		slidesToShow: 1
+		// 		}
+		// 	}
+		// ]
 	});
 
 	// Modal
